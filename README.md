bc5ce06999cb1de48d294c646498f95ec6d021c4
Lee Sharma <lee@leesharma.com>
2015-05-19 12:59:02 -0400

    update ubuntu versions and set up for heroku
    -add: PostgreSQL
    -add: Heroku toolbelt
    -change: README

--- README.md
+++ README.md
@@ -1,13 +1,48 @@
-## Script for OS X
-
-Copy and paste this line the Terminal and press Enter. Enjoy the text flying on the screen, it will take quite some time (approximately 30 minutes). Installer might ask for your administrator password. Grabbing a refreshing drink before starting is encouraged!
-
-    bash < <(curl -s https://raw.github.com/railsgirls/installation-scripts/master/rails-install-osx.sh)
+# Up and Running with Rails 4.2.1
 
 ## Script for Ubuntu
 
 To install Ruby on Rails development environment you just need to copy and paste this line the Terminal and press Enter. Enjoy the text flying on the screen, it will take quite some time. Grabbing a refreshing drink before starting is encouraged.
 
-    bash < <(curl -s  https://raw.github.com/railsgirls/installation-scripts/master/rails-install-ubuntu.sh)
+    $ bash < <(curl -s  https://raw.github.com/railsgirls/installation-scripts/master/rails-install-ubuntu.sh)
+
+## Next Steps
+
+### Set up Heroku
+
+- Create a [Heroku account][heroku]
+
+[heroku]: https://heroku.com/
+
+- Sign into Heroku toolbelt
+
+      $ heroku login
+
+### Install Zeal documentation
+
+Zeal is an offline documentation browser inspired by Dash, available for Linux and Windows. Install it [here][zeal].
+
+[zeal]: http://zealdocs.org
+
+### Set up a GitHub/BitBucket/GitLab account
+
+Github is the most popular option for open-source projects, but Gitlab and Bitbucket both 
+offer unlimited private repositories for personal work. 
+
+Personally, I use a GitHub account for open-source work and a GitLab account to backup my
+private repositories.
+
+### Start a new project
+
+    rails new *project_name* -d postgres
+    heroku create
+    git remote origin master *git@github.com:username/project_name.git*
+
+## Credit
+
+Thank you to @railsgirls for providing the script that is the basis of this project.
+
+
+
 
 
--- rails-install-ubuntu.sh
+++ rails-install-ubuntu.sh
@@ -6,21 +6,28 @@ echo "Updates packages. Asks for your password."
 sudo apt-get update -y
 
 echo "Installs packages. Give your password when asked."
-sudo apt-get install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev nodejs -y
+sudo apt-get install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 postgresql postgresql-contrib libpq-dev pgadmin3 libxml2-dev libxslt-dev autoconf libc6-dev nodejs -y
 
 echo "Installs ImageMagick for image processing"
 sudo apt-get install imagemagick --fix-missing -y
 
 echo "Installs RVM (Ruby Version Manager) for handling Ruby installation"
+****KEYS****
 curl -L get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 
 echo "Installs Ruby"
-rvm install 1.9.3-p547
-rvm use 1.9.3-p547 --default
+rvm install 2.2.1
+rvm use 2.2.1 --default
 
 gem install bundler --no-rdoc --no-ri
 gem install rails --no-rdoc --no-ri
+
+echo "Installs Heroku Toolbelt. Asks for your password."
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+echo "Installs PostgreSQL gem"
+gem install pg --no-rdoc --no-ri
 
 echo -e "\n- - - - - -\n"
 echo -e "Now we are going to print some information to check that everything is done:\n"
@@ -29,13 +36,13 @@ echo -e "Now we are going to print some information to check that everything is 
 # echo -n "Should be brew 0.8 or higher:       brew "
 # brew -v
 
-echo -n "Should be sqlite 3.7.3 or higher: sqlite "
+echo -n "Should be sqlite 3.8.2 or higher: sqlite "
 sqlite3 --version
-echo -n "Should be rvm 1.6.32 or higher:          "
+echo -n "Should be rvm 1.26.11 or higher:          "
 rvm --version | sed '/^.*$/N;s/\n//g' | cut -c 1-10
-echo -n "Should be ruby 1.9.3-p547:                "
+echo -n "Should be ruby 2.2.1:                "
 ruby -v | cut -d " " -f 2
-echo -n "Should be Rails 3.2.2 or higher:         "
+echo -n "Should be Rails 4.2.0 or higher:         "
 rails -v
 echo -e "\n- - - - - -\n"
 
@@ -46,11 +53,15 @@ and we will help you do the installation the manual way at the event.
 Congrats!
                                                                                  
 Make sure that all works well by running the application generator command:         
-    $ rails new railsgirls                                                       
+    $ rails new blog -d postgres                                                      
                                                                                  
 If you encounter the message:                                                    
     The program 'rails' is currently not installed.                              
                                                                                  
 It is just a hiccup with the shell, solutions:                                   
     $ source ~/.rvm/scripts/rvm                                                  
-    Allow login shell, example http://rvm.io/integration/gnome-terminal/"
+    Allow login shell, example http://rvm.io/integration/gnome-terminal/
+
+To set up Heroku, make an account at https://heroku.com/ and then run the following
+command (enter credentials when prompted):
+    $ heroku login"
