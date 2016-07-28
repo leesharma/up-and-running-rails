66f0b57eb4a02c3ce12d74023a1fa238fcc21930
Lee Sharma <lee@leesharma.com>
2016-07-28 16:39:12 -0400

    Install redis
    ActionCable requires redis, so now the script installs/checks the version for redis as well.

--- rails-install-osx.sh
+++ rails-install-osx.sh
@@ -26,6 +26,9 @@ echo "Install Ruby"
 rvm install 2.2.2
 rvm use 2.2.2 --default
 
+echo "Install Redis: in-memory data store for ActionCable"
+brew install redis
+
 gem install bundler --no-rdoc --no-ri
 gem install rails --no-rdoc --no-ri
 gem install pg  --no-rdoc --no-ri -- --with-pg-config=/Applications/Postgres.app/Contents/Versions/9.4/bin/pg_config
@@ -45,8 +48,12 @@ echo -n "Should be rvm 1.27.0 or higher:       "
 rvm --version | cut -d " " -f 1-2
 echo -n "Should be ruby 2.2.2:                 "
 ruby -v | cut -d " " -f 1-2
-echo -n "Should be Rails 5.0.0 or higher:      "
-rails -v
+echo -n "Should be rails 5.0.0 or higher:      "
+echo -n "rails "
+rails -v | cut -d " " -f 2
+echo -n "Should be redis 3.2.1 or higher:      "
+echo -n 'redis-server '
+redis-server --version | tr "=" " " | tr -s " " | cut -d " " -f 4
 echo -e "\n- - - - - -\n"
 
 echo "If the versions match, everything is installed correctly. If the versions
